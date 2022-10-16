a7820b407958898cd41f51721ee4490949fa5da1
Aaron Xu <ylxu@mit.edu>
2022-10-16 11:34:51 -0400

    update

--- configs/default_cifar10_configs.py
+++ configs/default_cifar10_configs.py
@@ -31,7 +31,7 @@ def get_default_configs():
 
   # evaluation
   config.eval = evaluate = ml_collections.ConfigDict()
-  evaluate.begin_ckpt = 10
+  evaluate.begin_ckpt = 8
   evaluate.end_ckpt = 26
   evaluate.batch_size = 1000
   evaluate.enable_sampling = False
