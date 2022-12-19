5ef334d9f8b8fb9f03501d82bf01eac5d05eea9d
MrBirrd <mathias.vogel@hotmail.ch>
2022-12-19 17:09:31 +0100

    Update job_slurm_training.sh

--- job_slurm_training.sh
+++ job_slurm_training.sh
@@ -3,5 +3,6 @@
 #SBATCH -n 8
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:16g
+#SBATCH --time=04:00:00
 
 python3 main.py --config ./configs/poisson/cifar10_ddpmpp.py --mode train --workdir poisson_ddpmpp
