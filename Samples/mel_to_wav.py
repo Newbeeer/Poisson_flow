264790a8875d449a8b586de5bf6debff42fcf54a
MrBirrd <mathias.vogel@hotmail.ch>
2022-12-28 11:42:51 +0100

    cleanup

--- Samples/mel_to_wav.py
+++ Samples/mel_to_wav.py
@@ -1,5 +1,4 @@
 import numpy as np
-import librosa.display
 from librosa.feature.inverse import mel_to_audio
 from librosa.feature.inverse import db_to_power
 import argparse
@@ -11,19 +10,22 @@ import matplotlib.pyplot as plt
 sample_rate = 16_000
 nfft = 1024
 torch_inverse = False
+plotting = False
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", required=True)
     args = parser.parse_args()
 
-    zfile = np.load(f"{args.dir}/samples_0.npz")
+    zfile = np.load(f"{args.dir}/sample.np")
     data = zfile.f.samples
 
     for i, im in enumerate(data):
         data = torch.from_numpy(im).type(torch.float)
-        plt.figure()
-        plt.imshow(data)
-        plt.show()
+        if plotting:
+            plt.figure()
+            plt.imshow(data)
+            plt.show()
         if torch_inverse:
         # change to channel first
             data = data.permute(2, 0, 1)
@@ -43,12 +45,12 @@ def main():
             mel_data = mel_data * 80
             mel_data -= mel_data.max()
             mel_data = db_to_power(mel_data)
-            audio = mel_to_audio(M=mel_data, sr=sample_rate,n_fft=1024, hop_length=256, center=True, power=1, n_iter=16)
-            
-        print(audio.shape)
-        plt.figure()
-        plt.plot(audio)
-        plt.show()
+            audio = mel_to_audio(M=mel_data, sr=sample_rate,n_fft=1024, hop_length=256, center=True, power=1, n_iter=64)
+        if plotting:
+            print(audio.shape)
+            plt.figure()
+            plt.plot(audio)
+            plt.show()
         sf.write(f"{args.dir}/audio/sample_{i}.wav", audio, 16_000, 'PCM_24')
 
 if __name__ == "__main__":
